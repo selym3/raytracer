262be0dd97cd94ac55c20e16772004784994e7cf
Myles Pasetsky <mp3blue99@gmail.com>
2021-07-27 19:56:26 -0400

    Remove old project

--- src/raytracing/Shape.cpp
+++
@@ -1,5 +0,0 @@
-#include "../../raytracing/Shape.hpp"
-
-Shape::Shape(const Material &color) : m_material{color} {}
-Shape::~Shape() {}
-const Material& Shape::material() const { return m_material; }
