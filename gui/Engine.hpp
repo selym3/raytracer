c146fa139466d338cff5e1c860c6f7fe5a161d14
Myles Pasetsky <mp3blue99@gmail.com>
2021-07-29 16:11:20 -0400

    Clean small part of engine code

--- gui/Engine.hpp
+++ gui/Engine.hpp
@@ -11,7 +11,47 @@ class Engine
     sf::RenderWindow window;
     Pixels buffer;
 
+private:
+
+    void handleEvent(const sf::Event& event)
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            window.close();
+        }
+        else if (event.type == sf::Event::KeyPressed)
+        {
+            if (event.key.code == sf::Keyboard::Escape)
+            {
+                window.close();
+            }
+
+            
+        }
+    }
+
+private:
+
+    void draw()
+    {
+        window.clear(sf::Color::White);
+
+        sf::Vector2<std::size_t> pixel(0, 0);
+        for (; pixel.y < buffer.get_height(); ++pixel.y) 
+        {
+            for (pixel.x = 0; pixel.x < buffer.get_width(); ++pixel.x)
+            {
+                auto r = rand() % 256;
+                buffer.set_color(pixel, sf::Color(r, r, r));
+            }
+        }
+
+        window.draw(buffer);
+        window.display();
+    }
+
 public:
+
     Engine(std::size_t width, std::size_t height) :
         window(
             sf::VideoMode(width, height),
@@ -32,36 +72,12 @@ public:
     void update()
     {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-            else if (event.type == sf::Event::KeyPressed)
-            {
-                if (event.key.code == sf::Keyboard::Escape)
-                {
-                    window.close();
-                }
-
-                
-            }
-        }
-
-        window.clear(sf::Color::White);
-
-        sf::Vector2<std::size_t> pixel(0, 0);
-        for (; pixel.y < buffer.get_height(); ++pixel.y) 
+        while (window.pollEvent(event)) 
         {
-            for (pixel.x = 0; pixel.x < buffer.get_width(); ++pixel.x)
-            {
-                auto r = rand() % 256;
-                buffer.set_color(pixel, sf::Color(r, r, r));
-            }
+            handleEvent(event);    
         }
 
-        window.draw(buffer);
-        window.display();
+        draw();
     }
 
 
