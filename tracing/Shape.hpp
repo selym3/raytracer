55af6de2e32e7a5601512799a6d3a7e61f1a5c7d
Myles Pasetsky <mp3blue99@gmail.com>
2021-08-06 15:46:17 -0400

    Add surface class, use colliders in shapes

--- tracing/Shape.hpp
+++ tracing/Shape.hpp
@@ -1,24 +1,51 @@
 #ifndef __SHAPE_HPP__
 #define __SHAPE_HPP__
 
+#include <optional>
+
 #include "Ray.hpp"
 #include "Geometry.hpp"
+
+struct Surface
+{
+    Vec3d color;
+    Collision collision;
+
+    Surface(const Vec3d& color, const Collision& collision) : 
+        color{color}, collision{collision}
+    {
+    }
+};
 
 struct Shape
 {
     // Data & Constructors
 
-    // Collider collider;
+    Collider collider;
+    Vec3d color;
 
-    // Shape(const Collider& collider) : 
-    //     collider { collider }
-    // {
-    // }
+    Shape(const Collider& collider, const Vec3d& color) : 
+        collider { collider }, color { color }
+    {
+    }
 
 public:
     // Ray Collisions
 
-    // Result collision(const )
+    std::optional<Surface> intersect(const Ray& ray) const
+    {
+        auto collision = collider(ray);
+        
+        if (!collision.has_value())
+            return std::nullopt;
+
+        Surface surface(
+            color,
+            collision.value()
+        );
+
+        return surface;
+    }
 
 
 };
